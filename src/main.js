dde08314b8ac1d2d3b5989d18991b70f128b341d
Rohith0696 <rohithusa96@gmail.com>
2020-03-06 22:26:03 -0600

    added code

---
+++ src/main.js
@@ -0,0 +1,44 @@
+const mul = (x, y, z) => { 
+  return x * y * z ;
+}
+
+
+const updateWithAdd = async (event) => {
+  
+   
+    const i = parseInt(document.querySelector('#length').value);
+    const j = parseInt(document.querySelector('#width').value);
+    const k = parseInt(document.querySelector('#height').value);
+    const ans = mul(i, j, k);
+    document.querySelector('#result').innerHTML = `Result is ${ans}`;
+  
+}
+
+document.addEventListener('click', event => {
+  if (event.target && event.target.id === 'btn') { updateWithAdd(event) }
+});
+
+
+const jokeURI = 'http://www.splashbase.co/api/v1/images/random';
+
+// fetch information
+const getJoke = async () => {
+    try {
+        const response = await fetch(jokeURI)
+        const obj = await response.json()
+        console.log(`FETCHED. Response JSON ${obj}`)
+        const joke = obj.url || 'No joke for you.'
+        return joke
+    } catch (error) { console.error(error) }
+}
+const updateWithJoke = async (event) => {
+    try {
+        document.querySelector('#imgarea').src = ''
+        const answer = await getJoke()
+        document.querySelector('#imgarea').src = answer
+    } catch (error) { console.error(error) }
+}
+// document.getElementById('getscore').onclick(getJoke);
+document.addEventListener('click', event => {
+    if (event.target && event.target.id === 'ajbtn') { updateWithJoke(event) }
+})
---
+++ test/main.test.js
@@ -0,0 +1,53 @@
+QUnit.module('MAIN MODULE', {})  // group all these tests together
+
+QUnit.test('TEST Volume of Rectangular Prism', assert => {
+  assert.equal(mul(0, 1, 1), 0, 'Zero')
+  assert.equal(mul(-1, -1, -1), -1, 'Negative integers')
+  assert.equal(mul(-1, 10, 10), -100, 'Negative value for length')
+  assert.equal(mul(10, -1, 10), -100, 'Negative value for width')
+  assert.equal(mul(10, 10, -1), -100, 'Negative value for height')
+})
+
+QUnit.config.autostart = false  // sync = false; start after loading html
+
+// This script, called when the page loads, reaches out to the app that we wish to test
+// It basically pastes the contents of that page into *this* web page, whew! This shows
+// how we manipulate the DOM.
+
+// The openingTag and closingTag specify which part of the original app's web page that we grab here
+// Pretty slick eh?
+
+window.addEventListener('load', () => {
+  const appURL = '../index.html' // reach out to the html for the app (js-gui)
+  const openingTag = '<main class="container mt-5 flex-fill">'
+  const closingTag = '</main>' // go grab it!
+  fetch(appURL, { method: 'GET' })
+    .then(response => {
+      return response.text() // returns promise
+    })
+    .then(txt => {                
+      const start = txt.indexOf(openingTag)
+      const end = txt.indexOf(closingTag) + closingTag.length
+      const html = txt.substring(start, end) // we only want part of the page
+      const qunitFixtureBody = document.querySelector('#qunit-fixture')
+      qunitFixtureBody.innerHTML = html // put the page into the DOM - the second div associated with this page
+      console.info(qunitFixtureBody) // print it out so we can see it (it doesn't get inserted into the page)
+      QUnit.start() // start the actual testing - it finds and runs both the tests, defined in QUnit.test()
+    })
+    .catch(error => {
+      console.error('error:', error);
+      QUnit.start()
+    })
+})
+
+// QUnit.test("TEST first number validation", assert => {
+//   const input = document.querySelector('#firstNumber')
+//   const warning = document.querySelector('#firstWarning')
+//   input.value = -3;
+//   assert.equal(input.value, -3, "Bad value assigned")
+//   assert.strictEqual(input.checkValidity(), false, "Correctly fails validation")
+//   input.focus()
+//   input.blur()
+//   assert.strictEqual(warning.innerHTML, 'Invalid input', `Correctly adds warning ${warning}`)
+// })
+
