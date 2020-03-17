6e8dceb20b7a67720f36eca442c81753a9aac4f3
Rohith0696 <rohithusa96@gmail.com>
2020-03-17 18:03:56 -0500

    added some changes

--- src/main.js
+++ src/main.js
@@ -11,6 +11,27 @@ const updateWithAdd = async (event) => {
     const k = parseInt(document.querySelector('#height').value);
     const ans = mul(i, j, k);
     document.querySelector('#result').innerHTML = `Result is ${ans}`;
+
+
+    
+    localStorage.setItem('length', i);
+    localStorage.setItem('width', j);
+    localStorage.setItem('height', k);
+    localStorage.setItem('volume', ans);
+
+    if (i != null) {
+      sessionStorage.setItem("length", i);
+    }
+    if (j != null) {
+      sessionStorage.setItem("width", j);
+    }
+    if (k != null) {
+      sessionStorage.setItem("height", k);
+    }
+    if (ans != null) {
+        sessionStorage.setItem("volume", ans);
+    }
+   
   
 }
 
@@ -22,7 +43,7 @@ document.addEventListener('click', event => {
 const jokeURI = "https://www.splashbase.co/api/v1/images/random";
 
 // fetch information
-const getJoke = async () => {
+const getImg = async () => {
     try {
         const response = await fetch(jokeURI)
         const obj = await response.json()
@@ -34,7 +55,7 @@ const getJoke = async () => {
 const updateWithJoke = async (event) => {
     try {
         document.querySelector('#imgarea').src = ''
-        const answer = await getJoke()
+        const answer = await getImg()
         document.querySelector('#imgarea').src = answer
     } catch (error) { console.error(error) }
 }
